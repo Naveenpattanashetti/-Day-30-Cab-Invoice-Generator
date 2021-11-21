cffbbbe1d2f5ea26bc68a011e12bec04071f6812
Naveen Pattanashetti <pattanashettinaveen.com>
2021-11-21 17:05:10 +0530

    Added:Wel come message to user

---
+++ src/main/java/com/bridgelabz/Cab_Invoice_Generator/InvoiceService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Cab_Invoice_Generator;
+
+public class InvoiceService {
+
+	public static void main(String[] args) {
+		System.out.println("wel come to cab invoice generator program");
+
+	}
+
+}
